b338168c31794780849993d6e0c53ad9c3e504f2
Maria Angelova <angelova.m@hotmail.com>
2023-06-16 14:04:50 +0200

    finalize the route summary

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import './views/list-view.js';
 import './views/placeholder-view.js';
 import './views/overlay-view.js';
 
-import {ApiService} from './services/api-service.js';
-
+import ApiService from './services/api-service.js';
 import AppModel from './models/app-model.js';
 
 import BriefPresenter from './presenters/brief-presenter.js';
@@ -18,18 +17,13 @@ import ListPresenter from './presenters/list-presenter.js';
 import PlaceholderPresenter from './presenters/placeholder-presenter.js';
 import OverlayPresenter from './presenters/overlay-presenter.js';
 
-const apiService = new ApiService({authorization: 'Basic lsdkgjsldkgjk'});
+const apiService = new ApiService({authorization: 'Basic abc123'});
 const appModel = new AppModel(apiService);
 
-new BriefPresenter(document.querySelector('brief-view'));
-new AddPresenter(document.querySelector('add-view'));
-new FilterPresenter(document.querySelector('filter-view'), appModel);
-new SortPresenter(document.querySelector('sort-view'), appModel);
-new ListPresenter(document.querySelector('list-view'), appModel);
 new PlaceholderPresenter(document.querySelector('placeholder-view'), appModel);
 
 appModel.load().then(() => {
-  new BriefPresenter(document.querySelector('brief-view'));
+  new BriefPresenter(document.querySelector('brief-view'), appModel);
   new AddPresenter(document.querySelector('add-view'));
   new FilterPresenter(document.querySelector('filter-view'), appModel);
   new SortPresenter(document.querySelector('sort-view'), appModel);
